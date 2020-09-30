eebcf016ee9539e16548ab09823c9ec9ca7bc079
CarmineD8 <carmine.recchiuto@dibris.unige.it>
2020-09-30 07:47:04 +0000

    Add files via upload

--- first_package/src/example.cpp
+++ first_package/src/example.cpp
@@ -1,18 +1,31 @@
 #include "ros/ros.h"
 #include "turtlesim/Pose.h"
 #include "geometry_msgs/Twist.h"
+#include "turtlesim/Spawn.h"
+#include "custom_messages/Two.h"
+#include "custom_messages/Sum.h"
 
 ros::Publisher pub;
+ros::Publisher pub2;
 
 
 void turtleCallback (const turtlesim::Pose::ConstPtr& msg){
 	ROS_INFO("Subscriber, %f, %f, %f", msg->x, msg->y, msg->theta);
-
-	geometry_msgs::Twist vel;
-	vel.angular.z = 0.1;
-	pub.publish(vel);
+	custom_messages::Two two;
+	two.a = msg->x;
+	two.b = msg->y;
+	pub2.publish(two);
 
 } 
+
+bool addCallback (custom_messages::Sum::Request& req, custom_messages::Sum::Response& res){
+	res.sum = req.a + req.b;
+	geometry_msgs::Twist vel;
+	vel.linear.x = req.a;
+	vel.angular.z = req.b;
+	pub.publish(vel);
+	return true;
+}
 
 
 
@@ -20,8 +33,19 @@ int main (int argc, char **argv){
 	ros::init(argc,argv,"example");
 	ros::NodeHandle nh;
 	
-	pub = nh.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel",1);
-	ros::Subscriber sub = nh.subscribe("/turtle1/pose",1,turtleCallback);
+	ros::ServiceClient client = nh.serviceClient<turtlesim::Spawn>("/spawn");
+	turtlesim::Spawn srv;
+	srv.request.x = 2.0;
+	srv.request.y = 2.0;
+	srv.request.name = "my_turtle";
+	client.call(srv);
+
+	pub = nh.advertise<geometry_msgs::Twist>("/my_turtle/cmd_vel",1);
+	pub2 = nh.advertise<custom_messages::Two>("/position",1);
+	
+	ros::ServiceServer service = nh.advertiseService("/sum", addCallback);
+
+	ros::Subscriber sub = nh.subscribe("/my_turtle/pose",1,turtleCallback);
 	ros::spin();
 	return 0;
 }
